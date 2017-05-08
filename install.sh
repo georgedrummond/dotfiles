2f3037c0872e388aa101e0b084dc0acff6412008
George Drummond <georgedrummond@gmail.com>
2017-05-08 14:28:50 -0400

    wip

--- install.sh
+++ install.sh
@@ -1,58 +1,83 @@
 set -e
-#
-# Install homebrew and dependencies
-#
-if ! type "brew" > /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
 
-brew tap homebrew/bundle
-brew bundle
+Running() {
+  echo "----> $1"
+}
+
+PrettyOutput() {
+  sed 's/^/      /' $1
+}
+
+# Install homebrew and dependencies
+
+    if ! type "brew" > /dev/null; then
+      echo "* Installing homebrew"
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    Running "Tapping homebrew/bundle"
+    brew tap homebrew/bundle | PrettyOutput
+
+    Running "Installing Homebrew Packages"
+    brew bundle | PrettyOutput #sed 's/^/      /'
 
 #
 # Set default gems for rbenv
 #
-ln -sf ~/dotfiles/rbenv/default-gems $(rbenv root)/default-gems
+
+    Running "Symlinking rbenv/default-gems"
+    ln -sf ~/dotfiles/rbenv/default-gems $(rbenv root)/default-gems
 
 #
 # Symlink all dotfiles
 #
-stow ack
-stow bash
-stow git
-stow pow
-stow ruby
-stow shell
-stow tmux
-stow tmuxinator
-stow vim
-stow zsh
+
+    Running "Symlinking all dotfiles with stow"
+    stow ack
+    stow bash
+    stow git
+    stow pow
+    stow ruby
+    stow shell
+    stow tmux
+    stow tmuxinator
+    stow vim
+    stow zsh
 
 #
 # Install oh-my-zsh
 #
-if [ ! -e ~/.oh-my-zsh ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+
+    Running "Installing oh-my-zsh"
+    if [ ! -e ~/.oh-my-zsh ]; then
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    fi
 
 #
 # Dracula theme for iterm2
 #
-mkdir -p ~/.iterm
-if [ ! -e ~/.iterm/dracula ]; then
-  git clone https://github.com/dracula/iterm.git ~/.iterm/dracula
-fi
 
-#
+    mkdir -p ~/.iterm
+    if [ ! -e ~/.iterm/dracula ]; then
+      git clone https://github.com/dracula/iterm.git ~/.iterm/dracula
+    else
+      cd ~/.iterm/dracula && git pull
+    fi
+
 #
 # Install tmuxinator
 #
-gem install tmuxinator
 
+    Running "gem install tmuxinator"
+    gem install tmuxinator | PrettyOutput
+
+#
 # Install vundle
 #
-if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-vim -c ":PluginInstall!" -c ":qa!" ~/.vimrc
 
+    if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
+      git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    else
+      cd ~/.vim/bundle/Vundle.vim && git pull
+    fi
+    vim -c ":PluginInstall!" -c ":qa!" ~/.vimrc
