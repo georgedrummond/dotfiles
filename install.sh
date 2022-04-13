38395d4578ff8da0d79ba741de9c5524422a1ee1
George Drummond <georgedrummond@gmail.com>
2022-04-13 15:18:57 -0400

    bashrc

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 
 export EDITOR='code --wait'
 
+rm ~/.bashrc
+
 ln -s $PWD/git/.gitconfig        ~/.gitconfig
 ln -s $PWD/git/.gitignore_global ~/.gitignore_global
-ln -s $PWD/shell/.bash_aliases   ~/.bash_aliases
+ln -s $PWD/shell/.bashrc         ~/.bashrc
